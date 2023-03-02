2eb9eb69981c6b86811b7a950f8bdec79a5bc9f9
Emre Cakiroglu <yunusemre34tr@gmail.com>
2023-03-02 15:03:35 +0300

    some theming for table

--- components/Table.tsx
+++ components/Table.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper } from "@mui/material";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, styled, tableCellClasses } from "@mui/material";
 import Papa from "papaparse";
 import styles from '@/styles/Home.module.css'
 
@@ -8,6 +8,25 @@ export default function CsvTable({csvUrl} : {csvUrl:string}){
   const [loading, setLoading] = useState<boolean>(true);
   const [error, setError] = useState<Error | null>(null);
 
+  const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+      backgroundColor: theme.palette.common.black,
+      color: theme.palette.common.white,
+    },
+    [`&.${tableCellClasses.body}`]: {
+      fontSize: 14,
+    },
+  }));
+
+  const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+      backgroundColor: theme.palette.action.hover,
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+      border: 0,
+    },
+  }));
   
   useEffect(() => {
     setLoading(true);
@@ -39,21 +58,21 @@ export default function CsvTable({csvUrl} : {csvUrl:string}){
 
   return (
     <TableContainer component={Paper}>
-      <Table>
+      <Table sx={{minWidth: 650}} size="small">
         <TableHead>
-          <TableRow>
+          <TableRow sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
             {Object.keys(tableData[0] || {}).map((header) => (
-              <TableCell key={header}>{header}</TableCell>
+              <StyledTableCell key={header}>{header}</StyledTableCell>
             ))}
           </TableRow>
         </TableHead>
         <TableBody>
           {tableData.map((row, index) => (
-            <TableRow key={index}>
+            <StyledTableRow key={index}>
               {Object.values(row).map((cell : any, index) => (
-                <TableCell key={index}>{cell}</TableCell>
+                <StyledTableCell key={index}>{cell}</StyledTableCell>
               ))}
-            </TableRow>
+            </StyledTableRow>
           ))}
         </TableBody>
       </Table>
