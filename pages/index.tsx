17339c001a91448ce4a4e4bf7a532bd399a8d577
Emre Cakiroglu <yunusemre34tr@gmail.com>
2023-04-23 18:20:25 +0300

    Fetch dates dynamically

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,28 +1,36 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
-import dates from 'public/dates.json'
+// import dates from 'public/dates.json'
 import course_codes from 'public/course_codes.json'
 import CsvTable from '@/components/Table'
 import DropdownSelect from '@/components/Select'
-import { Dispatch, SetStateAction, useState } from 'react'
-import { SelectChangeEvent } from '@mui/material'
+import { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import { Button, SelectChangeEvent, StepButton } from '@mui/material'
 
 const basePath = '/itu-web-archive'
 const url = 'https://raw.githubusercontent.com/keepdying/itu-web-archive/main/public/'
-const sortedDates = dates.sort((a, b) => {return a.label > b.label ? -1 : 1})
 
 export default function Home() {
   const [selectedDate, setSelectedDate] = useState('');
-  // const [link, setLink] = useState("");
   const [selectedCourse, setSelectedCourse] = useState('');
   const isSelectedAll = selectedDate !== '' && selectedCourse !== '';
-
+  const [dates, setDates] = useState<Array<{value: string, label: string}>>([]);
+  
   function getCsvLink(){
     if(isSelectedAll){
       return url + selectedDate + '/' + selectedCourse + '.csv'
     } else
     return ''
   }
+  
+  useEffect(() => {
+    fetch("https://raw.githubusercontent.com/keepdying/itu-web-archive/main/public/dates.json")
+      .then((response) => response.json())
+      .then((json) => setDates(json))
+      .catch((error) => setDates([{value: "", label: "Error fetching dates"}]));
+  }, []);
+
+  const sortedDates = dates.sort((a, b) => {return a.label > b.label ? -1 : 1});
 
   function handleChange(e: SelectChangeEvent<String>, setSelectedValue: Dispatch<SetStateAction<string>>){
     setSelectedValue(e.target.value as string);
